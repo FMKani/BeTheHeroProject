db241ee5115a011ecc98cedaf203f84b5c08c493
Wanielton F <wanieltonferreira@gmail.com>
2020-03-30 19:10:33 -0300

    Add api to the register file; obs: using axios as client

--- frontend/src/pages/NewIncident/index.js
+++ frontend/src/pages/NewIncident/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FiArrowLeft } from 'react-icons/fi'
+import { FiArrowLeft } from 'react-icons/fi';
 
 import './styles.css';
 import logo from '../../assets/logo.svg';
