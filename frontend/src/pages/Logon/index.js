1340ae883e2f3b640e335679809833834fb670a3
Wanielton F <wanieltonferreira@gmail.com>
2020-03-31 13:35:53 -0300

    Add api to the profile file

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,22 +1,45 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import {FiLogIn} from 'react-icons/fi';
 
+import api from '../../services/api';
 import './styles.css';
 
 import logo from '../../assets/logo.svg'
 import heroesImage from '../../assets/heroes.png'
 
 export default function Logon() {
+    const [id, setId] = useState('');
+    const history = useHistory();
+
+    async function handleLogon(e){
+        e.preventDefault();
+
+        try {
+            const response = await api.post('sessions', { id });
+
+            localStorage.setItem('ongId', id);
+            localStorage.setItem('ongName', response.data.name);
+
+            history.push('/profile');
+        } catch (error) {
+            alert('Falha no login');
+        }
+    }
+
     return (
         <div className="logon-container">
             <section className="form">
                 <img src={logo} alt="logo"/>
 
-                <form>
+                <form onSubmit={handleLogon}>
                     <h1>Faça seu logon</h1>
 
-                    <input placeholder="Sua ID"/>
+                    <input 
+                        placeholder="Sua ID"
+                        value={id}
+                        onChange = {e => setId(e.target.value)}
+                    />
                     <button className="button" type="submit">Entrar</button>
 
                     <Link className="back-link" to="/register">
