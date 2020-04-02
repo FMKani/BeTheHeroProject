2583f0fbd5c441b39ed9be529dfcf80d89b2b5a4
Wanielton F <wanieltonferreira@gmail.com>
2020-04-02 17:02:22 -0300

    Add the function to open whatsapp and gmail

--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -1,17 +1,38 @@
 import React from 'react';
 import { Feather } from '@expo/vector-icons';
-import { View, Text, Image, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { View, Text, Image, TouchableOpacity, Linking } from 'react-native';
+import * as MailCompose from 'expo-mail-composer';
 
 import styles from './styles';
 import logoImg from '../../assets/logo.png'
 
 export default function Detail() {
+    const navigation = useNavigation();
+    const message = 'Olá ONG, estou entrando em contato pois gostaria de ajuda no caso "CASO" com o valor de R$ 300,00';
+
+    function navigateBack() {
+        navigation.goBack();
+    }
+
+    function sendMail() {
+        MailCompose.composeAsync({
+            subject: 'Herói do caso: genti q coisa',
+            recipients: ['ongmail@needhelpppppppp.com'],
+            body: message,
+        })
+    }
+
+    function sendWhatsapp() {
+        Linking.openURL(`whatsapp://send?phone=5583993517273&text=${message}`);
+    }
+
     return(
         <View style={styles.container}>
             <View style={styles.header}>
                 <Image source={logoImg} />
                 
-                <TouchableOpacity onPress={() => {}}>
+                <TouchableOpacity onPress={navigateBack}>
                     <Feather name="arrow-left" size={28} color="#E02041"/>
                 </TouchableOpacity>
             </View>
@@ -34,11 +55,11 @@ export default function Detail() {
                 <Text style={styles.heroDescription}>Entre em contato:</Text>
 
                 <View style={styles.actions}>
-                    <TouchableOpacity style={styles.action} onPress={() => {}}>
+                    <TouchableOpacity style={styles.action} onPress={sendWhatsapp}>
                         <Text style={styles.actionText}>Whatsapp</Text>
                     </TouchableOpacity>
 
-                    <TouchableOpacity style={styles.action} onPress={() => {}}>
+                    <TouchableOpacity style={styles.action} onPress={sendMail}>
                         <Text style={styles.actionText}>Email</Text>
                     </TouchableOpacity>
                 </View>
